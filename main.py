888f5b2a60c3731633d035127f38806995093778
zensky123 <zensky@126.com>
2018-10-22 16:12:08 +0800

    Add SJF and SRTF algorithm

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from util.ran_data import generate_random_data
 from test.testFCFS import testFCFS
 from test.testRoundRobin import testRoundRobin
+from test.testSJF import testSJF
+from test.testSRTF import testSRTF
 
 if __name__ == '__main__':
     generate_random_data(50, 'default.dat')
     testFCFS('default.dat')
     testRoundRobin('default.dat')
+    testSJF('default.dat')
+    testSRTF('default.dat')
