2e98e0eb1734f3e5e7ca713545f9500b0d1703a0
zensky123 <zensky@126.com>
2018-10-22 16:21:34 +0800

    add HRRN algorithm

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from test.testFCFS import testFCFS
 from test.testRoundRobin import testRoundRobin
 from test.testSJF import testSJF
 from test.testSRTF import testSRTF
+from test.testHRRN import testHRRN
 
 if __name__ == '__main__':
     generate_random_data(50, 'default.dat')
@@ -10,3 +11,4 @@ if __name__ == '__main__':
     testRoundRobin('default.dat')
     testSJF('default.dat')
     testSRTF('default.dat')
+    testHRRN('default.dat')
